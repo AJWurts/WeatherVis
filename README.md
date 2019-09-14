e5870ab419c763a33e6f8f0e66a42f14eeba4b4d
Alexander Wurts <ajwurts@gmail.com>
2019-09-13 22:27:39 -0400

    Fixed bug where weather would cause slider to fail
    Former-commit-id: 6b8144bb62352ada8dad62b37bacee187c4a4417

--- README.md
+++ README.md
@@ -1,20 +1,12 @@
-# Weather Visualization using METARs
+# Weather Visualization using METARs and TAFs
+By Alexander Wurts
 
-tafDate.setUTCDate(16)
-1565970620413
-tafDate.setUTCHours(12)
-1565959820413
-date - tafDate
-10746046
-diff = new Date(date - tafDate)
-Wed Dec 31 1969 21:59:06 GMT-0500 (Eastern Standard Time)
-diff.getHours()
-21
-diff = date - tafDate
-10746046
-diff / 60 ** 3 // minutes
-49.75021296296296
-minutes = diff / 60 ** 3
-49.75021296296296
-seconds = Math.round(minutes)
-50
+## Data Source
+Data retrieved live from aviation weather services, parsed and displayed to the user.
+
+## Technology
+React and D3 frontend powered by a nodejs backend. Hosted on a Google Cloud App Engine.
+
+## Why?
+I am a private pilot and wanted an easier way to check the weather for an airport from my phone or computer.  
+This app visualizes the weather and allows anyone to search for an airport and see the most updated information. 
--- react-app/src/timeline/timeline.jsx
+++ react-app/src/timeline/timeline.jsx
@@ -6,19 +6,21 @@ import drawVis from './vis.jsx';
 import drawWeather from './weather.jsx';
 import { domainToASCII } from 'url';
 
-function timeFunc(start, end) {
+function timeFunc(start) {
     let startHour = +start.hour;
-    let endHour = 24 + end.hour;
 
     let fun = (full_time) => {
         let time = +full_time.hour + ((full_time.minute ? full_time.minute : 0) / 60);
-        if (+time > startHour && +full_time.day === +start.day + 1) {
-            return (time - startHour) + 24;
-        } else if (+time < startHour) {
-            return (24 - startHour) % 24 + time;
-        } else if (+time >= startHour) {
-            return +time - startHour;
+
+        let result;
+        if (full_time.day == start.day) {
+
+            result = (time - startHour);
+        } else { // (full_time.day > start.day) 
+            result = (24 - startHour) + time
         }
+
+        return result
     }
 
     return fun;
@@ -61,14 +63,23 @@ class TimeLine extends Component {
         }
 
         this.time = timeFunc(data.start, data.end)
-        let maxTime = (24 - data.start.hour) % 24 + data.end.hour;
+        let maxTime;
+        if (data.end.day == data.start.day) {
+
+            maxTime = (data.end.hour - data.start.hour);
+        } else { // (full_time.day > start.day) 
+            maxTime = (24 - data.start.hour) + data.end.hour
+        }
+
+        // console.log(data.start, data.end)
+
         this.maxTime = maxTime;
         let widthScale = d3.scaleLinear()
             .domain([0, maxTime])
             .range([width * 0.06, width]);
         // console.log("Width:", width)
         // console.log([0, (24 - data.start.hour) % 24 + data.end.hour])
-        console.log(data.forecast)
+        // console.log(maxTime, data.forecast)
 
         const vcSvg = d3.select(this.vcNode);
         vcSvg.selectAll('*').remove()
@@ -99,7 +110,6 @@ class TimeLine extends Component {
         // // this.tafAge = `TAF released ${hours}:${minutes} hours ago`
         this.state.tafAge = `TAF released ${Math.floor(hours)}:${("" + minutes).padStart(2, "0")} hours ago`
 
-        console.log(this.outer_svg)
         var outer_svg = d3.select(this.outer_svg)
 
         d3.selectAll(".tooltip").remove()
@@ -116,8 +126,8 @@ class TimeLine extends Component {
         outer_svg.selectAll("labels")
             .data([
                 ['time', height * 0.01],
-                ["clouds", height * 0.15], 
-                ["vis", height * 0.4], 
+                ["clouds", height * 0.15],
+                ["vis", height * 0.4],
                 ["wind", height * 0.65],
                 ["weather", height * 0.9]
             ]).enter()
@@ -133,70 +143,62 @@ class TimeLine extends Component {
 
     onMouseMove = (event) => {
 
+
         let invtime = d3.scaleLinear()
             .range([0, this.maxTime])
             .domain([this.width * 0.06, this.width]);
+
         var time = invtime(event.screenX);
-        console.log(time)
+
         let previous_data;
-        for (var i = this.props.data.forecast.length - 1; i >= 0 ; i--) {
+        for (var i = this.props.data.forecast.length - 1; i >= 0; i--) {
             if (this.time(this.props.data.forecast[i].from) <= time) {
                 previous_data = this.props.data.forecast[i];
                 break
             }
         }
-        console.log(i, previous_data)
-
         if (!previous_data) {
             return;
         }
 
+        // If they are the same only update. else do everything
+        if (this.last_data && this.last_data.from.hour !== previous_data.from.hour) {
+            let cloudStr = ""
+            for (let j = 5; j >= 0; j--) {
+                if (previous_data['skyc' + j]) {
+                    cloudStr += previous_data['skyc' + j] + " " + previous_data['skyl' + j] + " "
+                }
+            }
+            d3.select('.clouds')
+                .text(cloudStr)
+    
+            d3.select('.vis')
+                .text(previous_data.vsby + "SM")
+    
+            d3.select(".wind")
+                .text(`${previous_data.drct} at ${previous_data.sknt} KT ${previous_data.gust > 0 ? "gusting " + previous_data.gust : ""}`)
+    
+            d3.select('.weather')
+                .text(`${previous_data.weather.length >= 1 ? previous_data.weather[0].text : ""} ${previous_data.weather.length >= 2 ? previous_data.weather[1].text : ""}`)
+
+
+        } 
+        this.last_data = previous_data;
+ 
+
+
         d3.selectAll('.tooltip')
-            .attr('x', event.screenX + (this.maxTime - time < 10 ? -2 : 11))
-            .attr("text-anchor", this.maxTime - time < 10 ? 'end' : 'start')
+        .attr('x', event.screenX + (this.maxTime - time < 10 ? -2 : 11))
+        .attr("text-anchor", this.maxTime - time < 10 ? 'end' : 'start')
 
         d3.select('.ttbox')
             .attr('x', event.screenX)
-        
-
-        let cloudStr = ""
-        for (let j = 5; j >= 0; j--) {
-            if (previous_data['skyc' + j]) {
-                cloudStr += previous_data['skyc' + j]+ " "+ previous_data['skyl' + j] + " "
-            }
-        }
-        d3.select('.clouds')
-            .text(cloudStr)
-
-        d3.select('.vis')
-            .text(previous_data.vsby + "SM")
-
-        d3.select(".wind")
-            .text(`${previous_data.drct} at ${previous_data.sknt} KT ${previous_data.gust > 0 ? "gusting " + previous_data.gust : ""}`)
-
-        let weatherStr = ""
-        for (let j = 0; j < previous_data.weather.length; i++) {
-            weatherStr += previous_data.weather[j].text + " "
-        }
-        d3.select('.weather')
-            .text(previous_data.weater)
-
-        // If the next item exists and the next is smaller than the current, then day++
-        let day_plus_plus =  i + 1 < this.props.data.forecast.length && previous_data.from.hour > this.props.data.forecast[i+1].from.hour
-        console.log(day_plus_plus)
+    
         d3.select('.time')
-            .text(`Day ${day_plus_plus ? previous_data.from.day + 1: previous_data.from.day} Hour ${time.toFixed(0)}h ${previous_data.raw}`)
+            .text(`${previous_data.raw}`)
             .attr('alignment-baseline', 'hanging')
-            // .attr('text-align', 'start')
-
-
-        // event.getX
-        // event.getY
-        // want to show
-        // - relevant TAF section
-        // - aligned values for vis, wind, weather, clouds
-        // Need free floating box that moves to mouse X and set y coordinate
-
+        
+   
     }
     render() {
         var { width, height } = this.props;
