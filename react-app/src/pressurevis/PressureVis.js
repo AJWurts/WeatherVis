6965e43894fdf71a5181a65be052e6f3524d3ce0
Alexander Wurts <ajwurts@gmail.com>
2019-10-30 12:23:20 -0400

    Lauren's Grammer Fixes

--- react-app/src/pressurevis/PressureVis.js
+++ react-app/src/pressurevis/PressureVis.js
@@ -122,13 +122,26 @@ class App extends Component {
           <br></br>
           <br></br>
 
-          &emsp;Changes in pressure or temperature effect the density altitude.  The density altitude is calculated based on the current surface pressure, humidity, and temperature. The higher the density altitude the worse a plane performs. Set the temperature to 23˚C, 8˚C degrees above the standard atmosphere temperature. The density altitude at 1000ft above sea level (ASL) is a roughly 2000ft. That means taking off at an airport at 1000ft (ASL) will be the same as a 2000ft airport on standard day.
+          &emsp;Changes in pressure or temperature affect the density altitude. The density altitude is
+          calculated based on the current surface pressure, humidity, and temperature. The
+          higher the density altitude the worse a plane performs. Set the temperature to 23˚C, 8˚C
+          degrees above the standard atmosphere temperature. The density altitude at 1000ft
+          above sea level (ASL) is roughly 2000ft. That means taking off at an airport at 1000ft
+          (ASL) will be the same as a 2000ft airport on a standard day.
           <br></br>
           <br></br>
-          &emsp;To understand how the altimeter of a plane is affected click the checkbox to show a plane. When changing the temperature and pressure at the surface the plane's true altitude changes. This assumes the altimeter setting was not changed. This simulates flying between areas with different altimeter settingsWhen it is changed the correct altitude is maintained.  Since altimeters are just sensitive barometers (pressure sensors) they follow the pressure lines.
+          &emsp;To understand how the altimeter of a plane is affected click the checkbox to show a
+plane. When changing the temperature and pressure at the surface the plane&#39;s true
+altitude changes. This assumes the altimeter setting was not changed. This simulates
+flying between areas with different altimeter settings. When it is changed the correct
+altitude is maintained. Since altimeters are just sensitive barometers (pressure sensors)
+they follow the pressure lines.
           <br></br>
           <br></br>
-          &emsp;The visual assumes standard atmospheric lapse rate, in other words the temperature and pressure decrease as the altitude increases at a standard rate. The humidity is assumed to be 0%, however the humidity has a minial affect (+- 200ft) on density altitude.
+          &emsp;The visual assumes a standard atmospheric lapse rate, in other words, the
+temperature and pressure decrease as the altitude increases at a standard rate. The
+humidity is assumed to be 0%, however, the humidity has a minimal effect (+- 200ft) on
+density altitude.
 
         </div>
       </div>
