9e61c0f02a924fd163b1e25592a6bbd5235c6d26
Scott Peshak <scott@peshak.net>
2019-11-18 17:36:21 -0600

    Add airportdata object

---
+++ backend/airportdata.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+const AirportData = {
+  resolveIdent(airportId) {
+    if (airportId.slice(0, 1) === "K") {
+      return airportId;
+    }
+    if (airportId.length == 3) {
+      return "K" + airportId;
+    }
+    return airportId;
+  },
+
+  getCoordinates(airportId) {
+    return axios.get(`https://api.aeronautical.info/dev/?airport=${airportId}&include=geographic`)
+      .then(result => {
+        let airdata = result.data;
+
+        return {
+          lat: this.dmsToDec(airdata.latitude_dms),
+          lon: this.dmsToDec(airdata.longitude_dms),
+        }
+
+      }).catch(err => {
+        console.error(err);
+        return null
+      })
+  },
+
+
+  dmsToDec(value) {
+    let sign = 1
+
+    if(['W', 'S'].includes(value[value.length - 1])) {
+      sign = -1
+    }
+
+    let parts = value.split('-')
+
+    return sign * (parseInt(parts[0], 10) +
+      (parseInt(parts[1], 10) / 60) +
+      (parseFloat(parts[2], 10) / 3600))
+  }
+}
+
+module.exports = AirportData;
---
+++ backend/airportdata.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment node
+ */
+const AirportData = require('./airportdata');
+
+test('dmsToDec', () => {
+  expect(AirportData.dmsToDec('41-58-28.2790N')).toBeCloseTo(41.9744)
+  expect(AirportData.dmsToDec('41-58-28.2790S')).toBeCloseTo(-41.9744)
+  expect(AirportData.dmsToDec('087-54-23.7500W')).toBeCloseTo(-87.9065)
+  expect(AirportData.dmsToDec('087-54-23.7500E')).toBeCloseTo(87.9065)
+})
+
+
+test('getCoordinates', () => {
+  return AirportData.getCoordinates('KORD').then(data => {
+    expect(data).toEqual(expect.objectContaining({
+      lat: expect.any(Number),
+      lon: expect.any(Number)
+    }))
+  });
+})
+
+test('resolveIdent', () => {
+  expect(AirportData.resolveIdent('ORD')).toEqual('KORD');
+  expect(AirportData.resolveIdent('KORD')).toEqual('KORD');
+  expect(AirportData.resolveIdent('EGLL')).toEqual('EGLL');
+})
